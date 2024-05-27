1db394f6194c675956cc5acf62cad61ec09f3fdc
tahakayani <talaltaha253@gmail.com>
2024-05-27 13:14:26 +0500

    commit

---
+++ Web Project/public/homePage.js
@@ -0,0 +1,24 @@
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(function () {
+        slides[slideIndex - 1].classList.add("fade");
+    }, 200);
+    setTimeout(function () {
+        slides[slideIndex - 1].style.display = "none";
+        slides[slideIndex - 1].classList.remove("fade");
+        showSlides();
+    }, 4000);
+}
